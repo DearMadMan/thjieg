faac0a81406017983df7fd4c29f7098e7dc8da2e
dell <wangqiqi21@163.com>
2014-04-27 20:06:59 +0800

    DELL

--- inc/class/pagetool.class.php
+++ inc/class/pagetool.class.php
@@ -1,53 +1,18 @@
 <?php
 
-/**
- * 数据分页类  单例 类输出
- * 主要功能：
- * 1.提供分类页序列代码接口
- * 2.提供默认分页数打开页数据接口
- * 3.自动完成sql匹配
- * 4.自动获取数据总量
- * 5.提供获取单列名称接口
- *
- */
 
-/*
-使用方法:
-$pagetool=pagetool::GetInstance();
-if(!isset($_GET['pageNow']))
-{
-
-	$_GET['pageNow']=0;
-}
-$pageNow=intval($_GET['pageNow']);
-$pagePath='index.php';
-$dfNum=10;
-$sql="select * from ecs_users";
-$db=$GLOBALS['db'];
-$pagetool->setNeed($db,$sql,$dfNum,$pageNow,$pagePath,"user_id");
-$str= $pagetool->CreatePageRow();
-echo "$str";
-
-css:
-a{background: white; text-decoration: none;width: 50px;height: 20px; border: 1px solid red;color: #111;margin: 3px;padding:1px 3px;font-size: 12px;}
-    a:hover{background: red; border: 1px solid yellow;color:white;}
-    a.active{background: red; border: 1px solid yellow;color:white;}
-    a.disable{border: 1px solid #ccc; color:#ccc;background: none;}
-    a.disable:hover{border: 1px solid #ccc; color:#ccc;background: none;}
-    .rg{text-align: center;} //外层控制位置
- */
 
 
  class pagetool
 {
-	//设计为单例类
+
 	protected static $instance=null;
 	protected $db=null;
 	protected $sql="";
-	protected $dfNum=10; //默认每页显示的数据量
-	protected $count=0; //查询所得数据总量
-	protected $pageNow=0; //当前页码
-	protected $lastPage=0; //末页页码
+	protected $dfNum=10;
+	protected $count=0;
+	protected $pageNow=0;
+	protected $lastPage=0;
 	protected $pagePath="";
 
 	private function __construct()
@@ -65,15 +30,7 @@ a{background: white; text-decoration: none;width: 50px;height: 20px; border: 1px
 		return self::$instance;
 	}
 
-	/**
-	 * [setNeed 用于分页类初始化 不初始化无法进行后续操作]
-	 * @param [type] $db       [用于操作数据的数据库]
-	 * @param [type] $sql      [select * from users]
-	 * @param [type] $dfNum    [每页显示的数量]
-	 * @param [type] $pageNow  [当前为第几页从0开始]
-	 * @param [type] $pagePath [用于分页生成的调用页路径 如:user.php]
-	 * @param string $id       [数据库某一列的列名 如:users 表的 user_id]
-	 */
+
 	public function setNeed($db,$sql,$dfNum,$pageNow,$pagePath,$id="*")
 	{
 
@@ -101,11 +58,7 @@ a{background: white; text-decoration: none;width: 50px;height: 20px; border: 1px
 
 	}
 
-	/**
-	 * [getRes 根据当前页获取数据]
-	 * @param  [type] $pageNum [页码]
-	 * @return [type]          [数据]
-	 */
+
 	public function getRes()
 	{
 		if($this->pageNow>$this->lastPage)
@@ -121,9 +74,7 @@ a{background: white; text-decoration: none;width: 50px;height: 20px; border: 1px
 		return $res;
 	}
 
-	/**
-	 * [GetAll 返回包含所有数据的数组 非html数据]
-	 */
+
 	public function GetAll()
 	{
 		$arr=array();
@@ -138,9 +89,7 @@ a{background: white; text-decoration: none;width: 50px;height: 20px; border: 1px
 
 
 
-	/**
- 	* [CreatePageRow 用于生成分页HMLT代码]
- 	*/
+
 	public function CreatePageRow()
 	{
 		$str="";
